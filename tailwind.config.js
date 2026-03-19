1768cb0b561fb9f550b91c1f5dae46b1d35e45ea
lapbrian-4698 <lapbrian-4698@users.noreply.github.com>
2026-03-19 04:31:10 -0400

    Fix: Restauración de compilación Tailwind (Configuración movida a raíz)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx,css}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-        "./App.tsx",
-        "./index.tsx",
-        "./utils.ts",
-        "./constants.ts"
+        "./src/**/*.{js,ts,jsx,tsx,css,json}"
     ],
     darkMode: "class",
     theme: {
